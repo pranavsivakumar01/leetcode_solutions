744f34fe81fee9d9859713ed532262e0b39b0140
Pranav A S <pranavsivakumar328@gmail.com>
2025-01-20 21:53:56 +0530

    Time: 3 ms (100.00%) | Memory: 45.2 MB (12.57%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,26 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int[] sortByBits(int[] arr) {
-        // Convert int[] to Integer[] to use Comparator
-        Integer[] integerArr = Arrays.stream(arr)    // Convert int[] to Stream<Integer>
-                                      .boxed()        // Box the int values into Integer
-                                      .toArray(Integer[]::new); // Convert Stream<Integer> to Integer[]
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += Integer.bitCount(arr[i]) * 100000;
+        }
 
-        // Sort the Integer[] array with a custom comparator
-        Arrays.sort(integerArr, (a, b) -> {
-            int bitCountA = Integer.bitCount(a);  // Count 1 bits in a
-            int bitCountB = Integer.bitCount(b);  // Count 1 bits in b
+        Arrays.sort(arr);
 
-            if (bitCountA == bitCountB) {
-                return Integer.compare(a, b);  // If bit counts are equal, compare by value
-            }
-            return Integer.compare(bitCountA, bitCountB);  // Otherwise, compare by number of set bits
-        });
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] % 100000;
+        }
 
-        // Convert the Integer[] back to int[] before returning
-        return Arrays.stream(integerArr)
-                     .mapToInt(Integer::intValue)  // Convert Integer to int
-                     .toArray();  // Convert Stream<int> to int[]
+        return arr;
     }
 }
