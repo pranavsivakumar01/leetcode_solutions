cfc0ef963301e839f709153de3143eb914e9b323
Pranav A S <pranavsivakumar328@gmail.com>
2025-02-02 22:21:58 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (98.70%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
@@ -1,19 +1,11 @@
 class Solution {
     public int countSubarrays(int[] nums) {
-        int n=nums.length;
-        int c=0;
+        int n = nums.length;
+        int c = 0;
 
-        for(int i=0;i<n-3+1;i++)
-        {
-            for(int j=0;j<3;j++)
-            {
-                int sum=2*(nums[j+i] + nums[i+j+2]);
-
-                if(sum == nums[i+j+1])
-                {
-                    c++;
-                }
-                break;
+        for (int i = 0; i <= n - 3; i++) {
+            if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) {
+                c++;
             }
         }
         return c;
