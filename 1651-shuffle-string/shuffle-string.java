bf898945e4f71958ccd7f55542487fbf68c27de6
Pranav A S <pranavsivakumar328@gmail.com>
2025-03-09 23:03:47 +0530

    Time: 1 ms (73.03%) | Memory: 44.5 MB (76.11%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String restoreString(String s, int[] indices) {
         
         int length=s.length();
-        StringBuilder sb=new StringBuilder();
+        StringBuilder a=new StringBuilder();
          char c[]=new char[length];
        
         for(int i=0;i<length;i++){
@@ -10,7 +10,7 @@ class Solution {
             c[indices[i]]=s.charAt(i);
 
         }
-        sb.append(c);
-        return sb.toString();
+        a.append(c);
+        return a.toString();
     }
 }
